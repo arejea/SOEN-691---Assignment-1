6b7135c3be4456436f04753bca992fcdac48a9ce
arejea <adeyinka.areje@icloud.com>
2020-02-07 11:41:23 -0500

    Skeleton for plugin project created for team 5

---
+++ Team5 - Assignment1/src/team5__assignment1/patterns/ExceptionFinderTool.java
@@ -0,0 +1,7 @@
+package team5__assignment1.patterns;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+
+public class ExceptionFinderTool extends ASTVisitor {
+
+}
---
+++ Team5 - Assignment1/src/team5__assignment1/visitors/CatchClauseVisitor.java
@@ -0,0 +1,7 @@
+package team5__assignment1.visitors;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+
+public class CatchClauseVisitor extends ASTVisitor {
+
+}
