d4c62e55589352fd43b536bde0cfd138d3cbd7b6
arejea <47055032+arejea@users.noreply.github.com>
2020-02-10 20:40:07 -0500

    Update SampleHandler.java

--- Team5 - Assignment1/src/team5__assignment1/handlers/SampleHandler.java
+++ Team5 - Assignment1/src/team5__assignment1/handlers/SampleHandler.java
@@ -3,52 +3,49 @@ package team5__assignment1.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.console.ConsolePlugin;
-import org.eclipse.ui.console.IConsole;
-import org.eclipse.ui.console.IConsoleManager;
-import org.eclipse.ui.console.MessageConsole;
-import org.eclipse.ui.console.MessageConsoleStream;
 
-import team5__assignment1.patterns.ExceptionFinderTool;
+import org.eclipse.ui.IWorkbenchWindow;
 
-public class SampleHandler extends AbstractHandler {
-	
-		private static final String consoleName = "Team 5 Exception Anti Pattern Analysis Assignment Started......";
-		private static MessageConsole myConsole;
-		private static MessageConsoleStream output;
-	
-	
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
-		SampleHandler.myConsole = findConsole(consoleName);
-		SampleHandler.output = myConsole.newMessageStream();
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+
+
+
+	private static final String CONSOLE_NAME = " TEAM 5: DETECTING  OVERCATCH EXCEPTIONS AND RETRUN NULL" ;
+	private static MessageConsole myConsole;
+	private static MessageConsoleStream out;
 		
-		ExceptionFinderTool detectException = new ExceptionFinderTool();
-	//	detectException.execute(event);
-		return null;
-	}
-
-
-	private MessageConsole findConsole(String name) {
-		// TODO Auto-generated method stub
-		ConsolePlugin plugin = ConsolePlugin.getDefault();
-		IConsoleManager conMgr = plugin.getConsoleManager();
-		IConsole[] existingCon = conMgr.getConsoles();
 		
-		for(int i =0; i<existingCon.length; i++) {
-			if(name.equals(existingCon[i].getName())) {
-				return (MessageConsole) existingCon[i];			
-			}
+		@Override
+		public Object execute(ExecutionEvent event) throws ExecutionException {
+			SampleHandler.myConsole = findConsole(CONSOLE_NAME);
+		SampleHandler.out = myConsole.newMessageStream(); 
+			
+			
+			DetectCatchExceptions detectExceptions=new DetectCatchExceptions();
+			detectExceptions.execute(event);
+			
+			
+			return null;
 		}
 		
-		MessageConsole myNewConsole = new MessageConsole(name, null);
-		conMgr.addConsoles(new IConsole[] {myNewConsole});
+		private MessageConsole findConsole(String name) {
+			ConsolePlugin plugin = ConsolePlugin.getDefault();
+			IConsoleManager conMan = plugin.getConsoleManager();
+			IConsole[] existing = conMan.getConsoles();
+			
+			for (int i = 0; i < existing.length; i++)
+			   if (name.equals(existing[i].getName()))
+			      return (MessageConsole) existing[i];
+			
+			//no console found, so create a new one
+			//MessageConsole myConsole = new MessageConsole(name, null);
+			conMan.addConsoles(new IConsole[]{myConsole});
+			return myConsole;
+		}
 		
-		return myNewConsole;
-	}
-
-	static public void printMessage(String message) {
-		output.println(message);
-	}
-}
+		// To print messages into the Debug view, not just in the console here.
+		static public void printMessage(String message) {
+			out.println(message);
+}}
