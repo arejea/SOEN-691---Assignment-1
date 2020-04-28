0f557a5d331375f9fdba4f4e2d00b35bd125630a
Wedad Masoudi <wedad.m@gmail.com>
2020-04-27 20:22:54 -0400

    adding printing the data into a .csv file

--- ExtractMetrics/src/ReadFile.java
+++ ExtractMetrics/src/ReadFile.java
@@ -13,7 +13,7 @@ public class ReadFile {
   public static final String INPUT_FILE_POST_COMMITS = PROJECT_NAME + "_all_commits_after_release.txt";
   public static final String INPUT_FILE_PRE_COMMITS = PROJECT_NAME + "_all_commits_before_release.txt";
   public static final String INPUT_FILE_PRE_FILE_CHANGE = PROJECT_NAME + "_all_file_changes_before_release.txt";
-  public static final String OUTPUT_FILE = PROJECT_NAME + "_metrics.txt";
+  public static final String OUTPUT_FILE = PROJECT_NAME + "_metrics.csv";
 
   public static void main(String args[]) {
 
@@ -33,52 +33,27 @@ public class ReadFile {
     codeChurn_change = findCodeChurnAndChange(INPUT_FILE_PRE_FILE_CHANGE);
     System.out.println("Data Extracted!");
 
-    mergeData(unique_authors, codeChurn_change, post_release_bugs, pre_release_bugs);
+    Map<String, int[]> dataset;
+    dataset = mergeData(unique_authors, codeChurn_change, post_release_bugs, pre_release_bugs);
+
+    printResult(dataset);
     
   }
 
-  private static void mergeData(Map<String, Integer> authors, Map<String, int[]> changes, Map<String, Integer> post_bugs, Map<String, Integer> pre_bugs) {
-    Map<String, int[]> dataset = new HashMap<>();
+  // This method convert the input file containing issues to list
+  private static List<String> readBugs(String fileName) {
+    List<String> bugsList = new ArrayList<String>();
 
-    for (String file : authors.keySet()) {
-      //value[0]=number of authors, value[1]=number of code churn, value[2]=number of changes, value[3]=post-release bugs, value[4]=pre-release bugs
-      int[] values = new int[5];
-      values[0] = authors.get(file);
-      if (changes.containsKey(file)) {
-        values[1] = changes.get(file)[0];
-        values[2] = changes.get(file)[1];
-      }
-      if (post_bugs.containsKey(file))
-        values[3] = post_bugs.get(file);
-      if (pre_bugs.containsKey(file))
-        values[4] = pre_bugs.get(file);
-      dataset.put(file, values);
+    try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+      String str;
+      while ((str = br.readLine()) != null)
+        bugsList.add(str);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-
-    int post_bug = 0;
-    int pre_bug = 0;
-    int num_change = 0;
-
-    for(Map.Entry<String, int[]> entry : dataset.entrySet()){
-      if(entry.getValue()[3] != 0){
-        System.out.println(entry);
-        post_bug++;
-      }
-      if(entry.getValue()[4] != 0){
-        pre_bug++;
-      }
-      if(entry.getValue()[2] != 0){
-        num_change++;
-      }
-    }
-    System.out.println("Number of post-release bugs = " + post_bug);
-    System.out.println("Number of pre-release bugs = " + pre_bug);
-    System.out.println("Number of files have changed = " + num_change);
-    System.out.println("Done");
-
+    return bugsList;
   }
 
-// To do - add files without bug
   private static Map<String, Integer> findBugs(String bugFile, String changeFile) {
 
     List<String> bugList = readBugs(bugFile);
@@ -87,7 +62,7 @@ public class ReadFile {
       try (BufferedReader br = new BufferedReader(new FileReader(changeFile))) {
 
         String line;
-        Boolean isTargetChange = false;
+        boolean isTargetChange = false;
         while ((line = br.readLine()) != null) {
           if (line.contains("commit--") && line.contains(bug)) {
             isTargetChange = true;
@@ -150,19 +125,6 @@ public class ReadFile {
     }
   }
 
-  // This method convert the input file containing issues to list
-  private static List<String> readBugs(String fileName) {
-    List<String> bugsList = new ArrayList<String>();
-    try {
-      BufferedReader br = new BufferedReader(new FileReader(fileName));
-      String str;
-      while ((str = br.readLine()) != null)
-        bugsList.add(str);
-    } catch (IOException e) {
-    }
-    return bugsList;
-  }
-
   private static Map<String, int[]> findCodeChurnAndChange(String fileName){
 
     final Map<String, int[]> result = new HashMap<>();
@@ -189,7 +151,7 @@ public class ReadFile {
     return result;
   }
 
-  private static Map<String, int[]> updateCodeChurnAndChange(Map<String, int[]> result, String file, int codeChurn){
+  private static void updateCodeChurnAndChange(Map<String, int[]> result, String file, int codeChurn){
 
     if (result.containsKey(file)) {
       result.get(file)[0] = result.get(file)[0] + codeChurn;
@@ -200,7 +162,74 @@ public class ReadFile {
       value[1] = 1;
       result.put(file, value);
     }
-    return result;
+  }
+
+  private static Map<String, int[]> mergeData(Map<String, Integer> authors, Map<String, int[]> changes, Map<String, Integer> post_bugs, Map<String, Integer> pre_bugs) {
+    Map<String, int[]> dataset = new HashMap<>();
+
+    for (String file : authors.keySet()) {
+      //value[0]=number of authors, value[1]=number of code churn, value[2]=number of changes, value[3]=post-release bugs, value[4]=pre-release bugs
+      int[] values = new int[5];
+      values[0] = authors.get(file);
+      if (changes.containsKey(file)) {
+        values[1] = changes.get(file)[0];
+        values[2] = changes.get(file)[1];
+      }
+      if (post_bugs.containsKey(file))
+        values[3] = post_bugs.get(file);
+      if (pre_bugs.containsKey(file))
+        values[4] = pre_bugs.get(file);
+      dataset.put(file, values);
+    }
+
+    int post_bug = 0;
+    int pre_bug = 0;
+    int num_change = 0;
+
+    for(Map.Entry<String, int[]> entry : dataset.entrySet()){
+      if(entry.getValue()[3] != 0){
+        System.out.println(entry);
+        post_bug++;
+      }
+      if(entry.getValue()[4] != 0){
+        pre_bug++;
+      }
+      if(entry.getValue()[2] != 0){
+        num_change++;
+      }
+    }
+    System.out.println("Number of post-release bugs = " + post_bug);
+    System.out.println("Number of pre-release bugs = " + pre_bug);
+    System.out.println("Number of files have changed = " + num_change);
+    System.out.println("Done");
+    return dataset;
+  }
+
+  private static void printResult(Map<String, int[]> dataset) {
+
+    String eol = System.getProperty("line.separator");
+    try (FileWriter writer = new FileWriter(OUTPUT_FILE)){
+
+      for(Map.Entry<String, int[]> entry : dataset.entrySet()){
+        writer.append(entry.getKey())
+                .append(',')
+                .append(String.valueOf(entry.getValue()[0]))
+                .append(',')
+                .append(String.valueOf(entry.getValue()[1]))
+                .append(',')
+                .append(String.valueOf(entry.getValue()[2]))
+                .append(',')
+                .append(String.valueOf(entry.getValue()[3]))
+                .append(',')
+                .append(String.valueOf(entry.getValue()[4]))
+                .append(eol);
+      }
+    } catch (IOException ex) {
+      ex.printStackTrace(System.err);
+    }
+
+    //bw.write(String.format(entry.getKey(), entry.getValue()[0], entry.getValue()[1], entry.getValue()[2], entry.getValue()[3], entry.getValue()[4]));
+
   }
 
 }
